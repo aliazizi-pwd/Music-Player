396215f7371a71c404de2162da320eaead7d2813
Ali Azizi <aliazizi.4973@gmail.com>
2023-11-28 22:55:42 +0330

    select element HTML

--- source/JavaScript/app.js
+++ source/JavaScript/app.js
@@ -1,4 +1,18 @@
-// get start code javascript from music player...
-// select element document => $ 
+//* get start code javascript from music player...
+//* select element document => $ && select element let
 let $ = document;
-// select element 
+//* select element Const
+const coverMusic = $.querySelector(".image-cover");
+const audio = $.querySelector("audio");
+const progressTimeLine = $.querySelector(".progress-time-bar");
+const displayDurationTime = $.querySelector(".durationTime");
+const displayCurrentTime = $.querySelector(".currentTime");
+const nameSong = $.querySelector(".name-song");
+const nameSonger = $.querySelector(".name-songer");
+//* selecte button's music player
+const btnHeart = $.querySelector(".btn-heart");
+const btnPrevious = $.querySelector(".btn-Previous");
+const btnPlay = $.querySelector(".btn-Play");
+const btnNext = $.querySelector(".btn-next");
+const btnRepeat = $.querySelector(".btn-repeat");
+ 
