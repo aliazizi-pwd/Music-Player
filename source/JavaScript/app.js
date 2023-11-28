1369465ededb62439ca76049a03a94633fa3f483
Ali Azizi <aliazizi.4973@gmail.com>
2023-11-28 23:07:06 +0330

    Create List Music src-cover-name,...

--- source/JavaScript/app.js
+++ source/JavaScript/app.js
@@ -17,4 +17,34 @@ const btnPrevious = $.querySelector(".btn-Previous");
 const btnPlay = $.querySelector(".btn-Play");
 const btnNext = $.querySelector(".btn-next");
 const btnRepeat = $.querySelector(".btn-repeat");
- 
+
+//* Create list music player
+const listSong = [
+     {
+          id:1,
+          nameSong:"Nistametoon",
+          nameSonger:"MohammadReza Shayea",
+          src:"./music/01-Nistametoon.mp3",
+          coverIMG:"./public/image/img-cover-1.jpg",
+     },
+     {
+          id:2,
+          nameSong:"Khiz",
+          nameSonger:"MohammadReza Shayea",
+          src:"./music/01-Khiz.mp3",
+          coverIMG:"./public/image/img-cover-2.jpg",
+     },
+     {
+          id:3,
+          nameSong:"Khab Nabashim",
+          nameSonger:"MohammadReza Shayea",
+          src:"./music/03-Khab Nabashim.mp3",
+          coverIMG:"./public/image/img-cover-3.jpg",
+     }
+];
+
+
+
+// function getPlayMusicHandler () {
+//      btnPlay.innerHTML = `<i class="fa fa-3x fa-pause-circle"></i>`;
+// }
